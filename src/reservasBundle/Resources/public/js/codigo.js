0885a6ef9a44903b567a400643fc17b73483c5d3
Estela MC <Estela MC>
2017-05-14 22:33:58 +0200

    cambiar css, js y plantilla de reservas

--- src/reservasBundle/Resources/public/js/codigo.js
+++ src/reservasBundle/Resources/public/js/codigo.js
@@ -21,18 +21,13 @@ $(document).ready(function() {
 		format: 'dd/mm/yyyy',
 	  });
 
-    picker.set('disable', [
-    	//6, 7,
+    /*picker.set('disable', [    	
 	  // Using a collection of arrays formatted as [YEAR,MONTH,DATE]
 	  //[2017,4,9], [2017,4,13], [2017,4,20],
-
-	  // Using JavaScript Date objects
-	  //new Date(2017,9,13), new Date(2017,9,24)
-	  { from: [2017,4,1], to: [2017,4,20] }
 	]);
 
 	picker.set('enable', [
 	  [2017,4,9],
 	  new Date(2017,4,20)
-	]);
+	]);*/
 });
