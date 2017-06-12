59ba228acf76c6bd3cd4cb5d00c606524df4d1b6
Estelamc <estelamuco@gmail.com>
2017-06-12 23:25:21 +0200

    empezando con el manual de usuario

--- src/reservasBundle/Controller/AdminController.php
+++ src/reservasBundle/Controller/AdminController.php
@@ -506,6 +506,14 @@ class AdminController extends Controller
 
     }
 
+    public function manualAction(){
+      if (self::isAuthorized()==false) {
+          return $this->redirect('/admin/login');
+      }
+
+      return $this->render("reservasBundle:Admin:manual.html.twig");
+    }
+
 
 }
 
